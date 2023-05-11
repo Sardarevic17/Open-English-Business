9a573a08a38fabbd8189d3b13ea75591ac6b2763
Brian Sardarevic <sardarevicb@gmail.com>
2023-05-11 13:33:28 -0300

    subida de proyecto

---
+++ assets/config.js
@@ -0,0 +1,32 @@
+let config = {
+  "Title": "Your Name - Links",
+  "Name": "Your Name",
+  "Description": "Your Description",
+  "links": [
+    {
+      "Title": "Que es Open English Business",
+      "URL": "https://www.youtube.com/watch?v=unhZgKNAyTA",
+      "icon_classes": "fab fa-youtube",
+    },
+    {
+      "Title": "Open English Business",
+      "URL": "https://www.openenglish.com/para-empresas/",
+      "icon_classes": "fas fa-link"
+    },
+    {
+      "Title": "Next_U",
+      "URL": "https://www.nextu.com/",
+      "icon_classes": "fas fa-link"
+    },
+    {
+      "Title": "Agendamos reunion?",
+      "URL": "https://openenglish.na.chilipiper.com/book/me/brian-sardarevic?type=reunion-discovery",
+      "icon_classes": "fa fa-calendar",
+    },
+    {
+      "Title": "WhatsApp",
+      "URL": "https://wa.me/2944260005/",
+      "icon_classes": "fab fa-whatsapp"
+    },
+  ]
+}
